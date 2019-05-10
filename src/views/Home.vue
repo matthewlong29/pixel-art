33df644f78eea57510559b6fd8ab9daae9cdefd4
Matthew Long <matthewlong@Matthews-MacBook-Pro.local>
2019-05-10 19:04:24 -0400

    [removed] initial vue cli content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Pixel msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Pixel from '@/components/Pixel.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Pixel
   }
 }
 </script>
