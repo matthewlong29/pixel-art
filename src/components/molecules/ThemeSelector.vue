eb2751501df708fed6bdec3ab1e7e0f5229c6ca9
Matthew Long <long.matthew29@gmail.com>
2019-08-10 14:09:05 -0400

    added themes

--- src/components/molecules/ThemeSelector.vue
+++ src/components/molecules/ThemeSelector.vue
@@ -5,10 +5,62 @@
       <ToolHeader text="Theme" v-if="expanded" icon="minus-square" />
     </div>
     <div class="menu-content" v-if="expanded">
-      <input type="radio" id="dark" v-model="theme" value="dark" @change="submitChosenTheme()" />
-      <label for="dark">Dark</label>
-      <input type="radio" id="light" v-model="theme" value="light" @change="submitChosenTheme()" />
-      <label for="light">Light</label>
+      <div class="theme-option">
+        <label for="themeOne">
+          <input
+            type="radio"
+            id="themeOne"
+            v-model="theme"
+            value="themeOne"
+            @change="submitChosenTheme()"
+          />
+          <div style="background: #39414b"></div>
+          <div style="background: #1f2327"></div>
+          <div style="background: #4ecca3"></div>
+        </label>
+      </div>
+      <div class="theme-option">
+        <label for="themeTwo">
+          <input
+            type="radio"
+            id="themeTwo"
+            v-model="theme"
+            value="themeTwo"
+            @change="submitChosenTheme()"
+          />
+          <div style="background: #525252"></div>
+          <div style="background: #313131"></div>
+          <div style="background: #db5059"></div>
+        </label>
+      </div>
+      <div class="theme-option">
+        <label for="themeThree">
+          <input
+            type="radio"
+            id="themeThree"
+            v-model="theme"
+            value="themeThree"
+            @change="submitChosenTheme()"
+          />
+          <div style="background: #faf5ef"></div>
+          <div style="background: #d7d1c9"></div>
+          <div style="background: #672f2f"></div>
+        </label>
+      </div>
+      <div class="theme-option">
+        <label for="themeFour">
+          <input
+            type="radio"
+            id="themeFour"
+            v-model="theme"
+            value="themeFour"
+            @change="submitChosenTheme()"
+          />
+          <div style="background: #6e2142"></div>
+          <div style="background: #943855"></div>
+          <div style="background: #ffd692"></div>
+        </label>
+      </div>
     </div>
   </section>
 </template>
@@ -51,5 +103,26 @@ export default {
 section {
   @extend %menu-button;
   @extend %menu-content;
+  .theme-option {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    flex-direction: column;
+    label {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 4px 0px;
+      div {
+        display: inline-block;
+        padding: 8px 17px;
+        margin: 2px;
+        border: 1px solid black;
+      }
+      input {
+        margin: 0 5px 0 0;
+      }
+    }
+  }
 }
 </style>
